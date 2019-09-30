d6e9c45fdb68b333cda299400402bfb08ec119d0
yeomimKim <yeomeme@gmail.com>
2019-09-30 15:39:12 +0900

    리스트어댑터

--- app/src/main/java/kr/uncode/firebaselog/SearchListAdapter.java
+++ app/src/main/java/kr/uncode/firebaselog/SearchListAdapter.java
@@ -1,13 +1,18 @@
 package kr.uncode.firebaselog;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import kr.uncode.firebaselog.databinding.ListItemImageBinding;
 
 
 public class SearchListAdapter extends RecyclerView.Adapter<SearchListAdapter.SearchListViewHolder> {
@@ -38,25 +43,32 @@ public class SearchListAdapter extends RecyclerView.Adapter<SearchListAdapter.Se
     @Override
     public SearchListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        return null;
+        ListItemImageBinding binding = ListItemImageBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        binding.getRoot().setOnClickListener(v -> onAdapterItemClickListener.onAdapterViewClick(v));
+
+        return new SearchListViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull SearchListAdapter.SearchListViewHolder holder, int position) {
 
+        RetrofitResponse.Documents documents = data.get(position);
+        Glide.with(holder.binding.getRoot()).load(documents.image_url).into(holder.binding.ivImage);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
 
     public class SearchListViewHolder extends RecyclerView.ViewHolder {
 
+        ListItemImageBinding binding;
 
 
-        public SearchListViewHolder(@NonNull View itemView) {
-            super(itemView);
+        public SearchListViewHolder(@NonNull ListItemImageBinding itemView) {
+            super(itemView.getRoot());
+            binding = itemView;
         }
     }
 
